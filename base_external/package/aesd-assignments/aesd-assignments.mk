4f6b6b6ee4c1c4f801dacaa1e15ee61d8acb8b9b
Mikhail Tchernychev <mikhail_tchernychev@yahoo.com>
2026-09-13 11:38:01 -0700

    attempt to use https for assigment-3 access

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -10,7 +10,7 @@ AESD_ASSIGNMENTS_VERSION = a5d848ce6a33fbb17055288dc37ed1b6a0c2b990
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-mikhailtchernychev-dev
+AESD_ASSIGNMENTS_SITE = https://github.com/cu-ecen-aeld/assignments-3-and-later-mikhailtchernychev-dev.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
